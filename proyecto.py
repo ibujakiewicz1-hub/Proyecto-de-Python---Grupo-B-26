1177bb15528633f4902c723306395cc8cc694e75
ibujakiewicz1-hub <i.bujakiewicz1@gmail.com>
2026-07-03 18:12:52 -0300

    Inicio del proyecto
    Para arrancar definimos constantes, contadores, acumuladores y organizamos las funciones.

---
+++ proyecto.py
@@ -0,0 +1,41 @@
+# Tarifas
+TARIFA_MOTO = 500
+TARIFA_AUTO = 1000
+TARIFA_CAMION = 1500
+
+# Capacidad del estacionamiento
+CAPACIDAD = 50
+espacios_disponibles = CAPACIDAD
+
+# Estadísticas
+total_recaudado = 0
+total_vehiculos_atendidos = 0
+cantidad_autos = 0
+cantidad_motos = 0
+cantidad_camiones = 0
+
+# Registro de horarios
+horario_entrada = {}
+horario_salida = {}
+
+# Funciones del programa
+def mostrar_menu():
+    pass
+
+def ingresar_vehiculo():
+    pass
+
+def retirar_vehiculo():
+    pass
+
+def mostrar_vehiculos():
+    pass
+
+def mostrar_estadisticas():
+    pass
+
+def espacios_disponibles():
+    pass
+
+def buscar_vehiculo():
+    pass
